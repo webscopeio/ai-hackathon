6b3db4d85b3d914d44860e15cf2b79e97d59353d
eric valcik <eric.valcik@gmail.com>
2025-04-03 21:06:25 +0200

    fix tsconfig

--- internal/repository/validate/nodeTemplate/playwright.config.ts
+++ internal/repository/validate/nodeTemplate/playwright.config.ts
@@ -3,9 +3,6 @@ import { defineConfig, devices } from "@playwright/test";
 export default defineConfig({
   testDir: "./tests",
   fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
   use: {
     baseURL: "https://www.google.com",
     trace: "on-first-retry",
